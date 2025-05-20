010024a98eb6fa78d98ef6ec8dd8e23281542e6c
uno/hurtki <asboba2101@gmail.com>
2025-05-20 16:45:50 +0300

    add settings to make django create new staticfiles when collect static done to escape caching them on the client

--- shop_tg_app/shop_tg_app/settings.py
+++ shop_tg_app/shop_tg_app/settings.py
@@ -147,3 +147,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage',
+    },
+}
